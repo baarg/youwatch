4a3bcf8c9dacf27e8070521edc7fc530c9d42458
AhmadEleiwa <43647315+baarg@users.noreply.github.com>
2020-07-07 20:26:57 +0300

    Update settings.py

--- youwatch/settings.py
+++ youwatch/settings.py
@@ -134,4 +134,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
